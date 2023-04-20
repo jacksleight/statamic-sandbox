82af8656a64eea1bf7b8e607f0c88cddb656326f
Jack Sleight <jacksleight@gmail.com>
2023-04-20 14:36:32 +0100

    Commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use JackSleight\StatamicBardMutator\Facades\Mutator;
+use JackSleight\StatamicBardMutator\Support\Data;
 use Statamic\Statamic;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +16,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            \Tiptap\Editor::class,
+            \JackSleight\StatamicBardMutator\Editor::class
+        );
     }
 
     /**
@@ -26,5 +31,33 @@ class AppServiceProvider extends ServiceProvider
     {
         Statamic::script('app', 'cp');
         Statamic::style('app', 'cp');
+
+        // Mutator::html('paragraph', function ($value, $data) {
+        //     if (($data->content[0]->type ?? null) === 'image') {
+        //         return null;
+        //     }
+
+        //     return $value;
+        // });
+
+        // Mutator::html('paragraph', function ($value, $meta) {
+        //     if (($meta['parent']->type ?? null) === 'listItem') {
+        //         return null;
+        //     }
+
+        //     return $value;
+        // });
+
+        // Mutator::data('heading', function ($data) {
+        //     $slug = str_slug(collect($data->content)->implode('text', ''));
+        //     array_unshift(
+        //         $data->content,
+        //         Data::html('<a id="'.$slug.'" href="#'.$slug.'">#</a>')
+        //     );
+        // });
+
+        // Mutator::html('image', function ($value) {
+        //     return ['content' => view('partials/image', $value[1])];
+        // });
     }
 }
